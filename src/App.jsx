54182dd9e1cb6189b0ad79944a7e49c43fd35fa2
Haojie Zhang <zhj3408237@gmail.com>
2024-06-08 15:17:02 -0400

    UI design completed

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { Route, Routes } from "react-router-dom";
+
+import HomePage from "./pages/HomePage";
+import Sidebar from "./components/Sidebar";
+import FavoritesPage from "./pages/FavoritesPage";
+
+const App = () => {
+  return (
+    <div className="flex">
+      <Sidebar />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/favorites" element={<FavoritesPage />} />
+      </Routes>
+    </div>
+  );
+};
+
+export default App;
